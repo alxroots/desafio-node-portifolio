4285897a46658dea69842af4078b58d3c9ccd00f
Alex Sousa <alxroots@gmail.com>
2020-10-28 06:30:34 -0300

    Finished First Challenge Rocketseat - NODEJS

--- src/app.js
+++ src/app.js
@@ -1,33 +1,80 @@
 const express = require("express");
 const cors = require("cors");
+const { v4: uuid } = require('uuid');
 
-// const { v4: uuid } = require('uuid');
 
 const app = express();
+
 
 app.use(express.json());
 app.use(cors());
 
+
 const repositories = [];
 
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  
+  return response.json(repositories)
 });
+
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const {title, url, techs} = request.body;
+  
+  const newRepo = { id: uuid(), title, url, techs, likes:0 }
+  repositories.push(newRepo)
+  
+  return response.status(201).json(newRepo)
 });
+
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  if (repoIndex < 0 ){
+    return response.status(400).json({ error: 'Repository not found' })
+  }
+  
+  const { title, url, techs } = request.body;
+  const repo = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[repoIndex].likes,
+  };
+  repositories[repoIndex] = repo
+
+  return response.json(repo)
 });
+
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+  if (repoIndex < 0 ){
+    return response.status(400).json({ error: 'Repository not found' })
+  }
+
+  repositories.splice(repoIndex, 1)
+  return response.status(204).send()
+
 });
 
+
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repo = repositories.find(repo => repo.id === id);
+  if (!repo){
+    return response.status(400).send()
+  }
+
+  repo.likes += 1;
+  
+  return response.status(201).json(repo)  
+
 });
+
 
 module.exports = app;
